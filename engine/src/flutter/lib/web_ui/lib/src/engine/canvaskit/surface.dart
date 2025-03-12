7cb42fedcdbfc2b44f6564bcc9b1e341e5c8fb8d
Harry Terkelsen <1961493+harryterkelsen@users.noreply.github.com>
2025-03-12 07:19:14 -0700

    [stable] Cherrypick fix for stretched embedded app on mobile (#164406)
    What is the link to the issue this cherry-pick is addressing?
    
    https://github.com/flutter/flutter/issues/159974
    
    Explain this cherry pick in one line that is accessible to most Flutter developers. See [best practices](https://github.com/flutter/flutter/blob/main/docs/releases/Hotfix-Documentation-Best-Practices.md) for examples
    
    Improve performance of CanvasKit by avoiding copying bitmaps when rendering
    
    What is the impact (ex. visual jank on Samsung phones, app crash, cannot ship an iOS app)? Does it impact development (ex. flutter doctor crashes when Android Studio is installed), or the shipping production app (the app crashes on launch)
    
    Improves performance of all apps using CanvasKit. Fixes a bug relating to embedded Flutter apps on mobile.
    
    Is there a workaround for this issue?
    
    No
    
    What is the risk level of this cherry-pick?
    
    Are you confident that your fix is well-tested by automated tests?
    
    What are the steps to validate that this fix works?
    
    Run the example app in the fixed issue on Chrome for Android and verify it works as expected.

--- engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/surface.dart
+++ engine/src/flutter/lib/web_ui/lib/src/engine/canvaskit/surface.dart
@@ -23,6 +23,27 @@ import 'util.dart';
 // removes the ability for disabling AA on Paint objects.
 const bool _kUsingMSAA = bool.fromEnvironment('flutter.canvaskit.msaa');
 
+typedef SubmitCallback = bool Function(SurfaceFrame, CkCanvas);
+
+/// A frame which contains a canvas to be drawn into.
+class SurfaceFrame {
+  SurfaceFrame(this.skiaSurface, this.submitCallback) : _submitted = false;
+
+  final CkSurface skiaSurface;
+  final SubmitCallback submitCallback;
+  final bool _submitted;
+
+  /// Submit this frame to be drawn.
+  bool submit() {
+    if (_submitted) {
+      return false;
+    }
+    return submitCallback(this, skiaCanvas);
+  }
+
+  CkCanvas get skiaCanvas => skiaSurface.getCanvas();
+}
+
 /// A surface which can be drawn into by the compositor.
 ///
 /// The underlying representation is a [CkSurface], which can be reused by
@@ -33,19 +54,6 @@ class Surface extends DisplayCanvas {
     : useOffscreenCanvas = Surface.offscreenCanvasSupported && !isDisplayCanvas;
 
   CkSurface? _surface;
-
-  /// Returns the underlying CanvasKit Surface. Should only be used in tests.
-  CkSurface? debugGetCkSurface() {
-    bool assertsEnabled = false;
-    assert(() {
-      assertsEnabled = true;
-      return true;
-    }());
-    if (!assertsEnabled) {
-      throw StateError('debugGetCkSurface() can only be used in tests');
-    }
-    return _surface;
-  }
 
   /// Whether or not to use an `OffscreenCanvas` to back this [Surface].
   final bool useOffscreenCanvas;
@@ -103,7 +111,7 @@ class Surface extends DisplayCanvas {
 
   /// The <canvas> backing this Surface in the case that OffscreenCanvas isn't
   /// supported.
-  DomHTMLCanvasElement? _canvasElement;
+  DomCanvasElement? _canvasElement;
 
   /// Note, if this getter is called, then this Surface is being used as an
   /// overlay and must be backed by an onscreen <canvas> element.
@@ -153,7 +161,7 @@ class Surface extends DisplayCanvas {
       if (useOffscreenCanvas) {
         bitmap = _offscreenCanvas!.transferToImageBitmap();
       } else {
-        bitmapSource = _canvasElement!;
+        bitmapSource = _canvasElement! as JSObject;
         bitmap = await createImageBitmap(bitmapSource, (
           x: 0,
           y: _pixelHeight - bitmapSize.height,
@@ -173,6 +181,20 @@ class Surface extends DisplayCanvas {
       }
       canvas.renderWithNoBitmapSupport(imageSource, _pixelHeight, bitmapSize);
     }
+  }
+
+  /// Acquire a frame of the given [size] containing a drawable canvas.
+  ///
+  /// The given [size] is in physical pixels.
+  SurfaceFrame acquireFrame(ui.Size size) {
+    final CkSurface surface = createOrUpdateSurface(BitmapSize.fromSize(size));
+
+    // ignore: prefer_function_declarations_over_variables
+    final SubmitCallback submitCallback = (SurfaceFrame surfaceFrame, CkCanvas canvas) {
+      return _presentSurface();
+    };
+
+    return SurfaceFrame(surface, submitCallback);
   }
 
   BitmapSize? _currentCanvasPhysicalSize;
@@ -272,9 +294,9 @@ class Surface extends DisplayCanvas {
       }
     }
 
-    // If we reached here, then this is the first frame and we haven't made a
-    // surface yet, we are forcing a new context, or the size of the surface
-    // has changed and we need to make a new one.
+    // If we reached here, then either we are forcing a new context, or
+    // the size of the surface has changed so we need to make a new one.
+
     _surface?.dispose();
     _surface = null;
 
@@ -291,7 +313,7 @@ class Surface extends DisplayCanvas {
     return _surface = _createNewSurface(size);
   }
 
-  void _contextRestoredListener(DomEvent event) {
+  JSVoid _contextRestoredListener(DomEvent event) {
     assert(
       _contextLost,
       'Received "webglcontextrestored" event but never received '
@@ -304,7 +326,7 @@ class Surface extends DisplayCanvas {
     event.preventDefault();
   }
 
-  void _contextLostListener(DomEvent event) {
+  JSVoid _contextLostListener(DomEvent event) {
     assert(
       event.target == _offscreenCanvas || event.target == _canvasElement,
       'Received a context lost event for a disposed canvas',
@@ -323,13 +345,9 @@ class Surface extends DisplayCanvas {
       _offscreenCanvas!.removeEventListener(
         'webglcontextrestored',
         _cachedContextRestoredListener,
-        false.toJS,
+        false,
       );
-      _offscreenCanvas!.removeEventListener(
-        'webglcontextlost',
-        _cachedContextLostListener,
-        false.toJS,
-      );
+      _offscreenCanvas!.removeEventListener('webglcontextlost', _cachedContextLostListener, false);
       _offscreenCanvas = null;
       _cachedContextRestoredListener = null;
       _cachedContextLostListener = null;
@@ -337,13 +355,9 @@ class Surface extends DisplayCanvas {
       _canvasElement!.removeEventListener(
         'webglcontextrestored',
         _cachedContextRestoredListener,
-        false.toJS,
+        false,
       );
-      _canvasElement!.removeEventListener(
-        'webglcontextlost',
-        _cachedContextLostListener,
-        false.toJS,
-      );
+      _canvasElement!.removeEventListener('webglcontextlost', _cachedContextLostListener, false);
       _canvasElement!.remove();
       _canvasElement = null;
       _cachedContextRestoredListener = null;
@@ -364,7 +378,7 @@ class Surface extends DisplayCanvas {
       _offscreenCanvas = offscreenCanvas;
       _canvasElement = null;
     } else {
-      final DomHTMLCanvasElement canvas = createDomCanvasElement(
+      final DomCanvasElement canvas = createDomCanvasElement(
         width: _pixelWidth,
         height: _pixelHeight,
       );
@@ -387,8 +401,8 @@ class Surface extends DisplayCanvas {
     // See also: https://www.khronos.org/webgl/wiki/HandlingContextLost
     _cachedContextRestoredListener = createDomEventListener(_contextRestoredListener);
     _cachedContextLostListener = createDomEventListener(_contextLostListener);
-    htmlCanvas.addEventListener('webglcontextlost', _cachedContextLostListener, false.toJS);
-    htmlCanvas.addEventListener('webglcontextrestored', _cachedContextRestoredListener, false.toJS);
+    htmlCanvas.addEventListener('webglcontextlost', _cachedContextLostListener, false);
+    htmlCanvas.addEventListener('webglcontextrestored', _cachedContextRestoredListener, false);
     _forceNewContext = false;
     _contextLost = false;
 
@@ -411,8 +425,6 @@ class Surface extends DisplayCanvas {
       if (_glContext != 0) {
         _grContext = canvasKit.MakeGrContext(glContext.toDouble());
         if (_grContext == null) {
-          // TODO(harryterkelsen): Make this error message more descriptive by
-          // reporting the number of currently live Surfaces, https://github.com/flutter/flutter/issues/162868.
           throw CanvasKitError(
             'Failed to initialize CanvasKit. '
             'CanvasKit.MakeGrContext returned null.',
@@ -488,6 +500,11 @@ class Surface extends DisplayCanvas {
     }
   }
 
+  bool _presentSurface() {
+    _surface!.flush();
+    return true;
+  }
+
   @override
   bool get isConnected => _canvasElement!.isConnected!;
 
@@ -498,15 +515,11 @@ class Surface extends DisplayCanvas {
 
   @override
   void dispose() {
-    _offscreenCanvas?.removeEventListener(
-      'webglcontextlost',
-      _cachedContextLostListener,
-      false.toJS,
-    );
+    _offscreenCanvas?.removeEventListener('webglcontextlost', _cachedContextLostListener, false);
     _offscreenCanvas?.removeEventListener(
       'webglcontextrestored',
       _cachedContextRestoredListener,
-      false.toJS,
+      false,
     );
     _cachedContextLostListener = null;
     _cachedContextRestoredListener = null;
